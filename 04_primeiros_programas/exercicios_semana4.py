08f93be9190d756dc111e3569875d6992df315f8
GNicodemo <gustavo.nicodemo@hotmail.com>
2026-02-20 17:01:31 -0300

    Exercicios Semana 4
    Conclusão da semana 4 do curso

---
+++ 04_primeiros_programas/exercicios_semana4.py
@@ -0,0 +1,69 @@
+## 3.1
+
+try:
+    temp_c = float(input("Digite a temperatura em graus Fahrenheit: "))
+    temp_f = (temp_c * 9/5) + 32
+
+    print(f"A temperatura em graus Celsius é {temp_f:.2f}")
+
+except ValueError:
+
+    print("Erro: Por favor, digite apenas números válidos.")
+
+## 3.8
+
+def media(numbers):
+    return sum(numbers) / len(numbers)
+
+numeros = [3, 3, 5]
+
+print(f"{media(numeros):.3f}")
+
+##3.9
+import math
+
+def perimetro(f_raio):
+    f_perimetro = 2 * math.pi * f_raio
+    return f_perimetro
+
+raio = 1
+
+print(f"perimetro = {perimetro(raio):.4f}")
+
+##3.10
+
+def negativos(f_numeros):
+
+    for i in range(len(f_numeros)):
+        if f_numeros[i] < 0:
+            print(f_numeros[i])
+
+numeros = [-1, 2, -3, 4,-5]
+negativos(numeros)
+
+##3.11 e 3.12
+
+def troca_posicao(lista):
+    temp = lista[0]
+    lista[0] = lista[-1]
+    lista[-1] = temp
+
+    print(lista)
+
+time = ["Ava", "Eleanor", "Clare", "Sarah"]
+troca_posicao(time)
+
+ingredientes = ["farinha", "açúcar", "manteiga", "maçãs"]
+troca_posicao(ingredientes)
+
+##Entendendo alocação de Listas
+
+lista1 = [1, 2, 3]
+lista2 = lista1
+
+lista1.append(10)
+
+lista1 = [5, 8, 9]
+
+print(lista1)
+print(lista2)
---
+++ 04_primeiros_programas/print_input_eval_.py
@@ -0,0 +1,50 @@
+h = 13
+print("Agora são", h, 'horas', sep=".")
+
+print("Agora são", h, 'horas')
+print("Agora são", h, 'horas', end=">")
+print("Agora são", h, 'horas', end=".\n\n")
+
+print("Agora são", h, 'horas')
+
+##
+
+x = input ("Digite um numero: ")
+
+print(type(x))
+
+x = eval(x)
+
+print(type(x))
+
+print(x)
+
+## Calculo Temperatura de Celsius para Farenheint
+
+temp_c = eval(input("Digite a temp. em Celsius: "))
+temp_f = (temp_c * 9/5) + 32
+temp_k = temp_c + 273.15
+
+print("Temperatura em Farenheint: ", temp_f,"\nTemperatura em Kelvin: ", temp_k)
+##
+
+def media(x, y, z):
+    return (x + y + z) / 3
+
+
+try:
+    # Caso queira testar com entrada do usuário, use:
+    # x = float(input("Digite o primeiro número: "))
+    # y = float(input("Digite o segundo número: "))
+    # z = float(input("Digite o terceiro número: "))
+
+    x = 1
+    y = 1
+    z = 2
+
+    resultado = media(x, y, z)
+    # Correção: A aspa final deve vir APÓS o fechamento da chave }
+    print(f"A média dos números é: {resultado:.3f}")
+
+except ValueError:
+    print("Erro: Por favor, digite apenas números válidos.")
